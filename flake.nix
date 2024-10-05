016a6d6f7e80c5f9c53381e291a4b0f1f2d0921f
Jonathan Koff <jonathan@jkoff.ca>
2024-10-05 17:23:08 +0200

    Remove nix files

--- flake.nix
+++ flake.nix
@@ -12,15 +12,6 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        packages.default = pkgs.crystal.buildCrystalPackage {
-          pname = "mastoproxy";
-          version = "0.1.0";
-          src = ./.;
-          crystal = pkgs.crystal;
-          shardsFile = ./shards.nix;
-          crystalBinaries.mastoproxy.src = "src/main.cr";
-        };
-
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             crystal
--- shards.nix
+++
@@ -1,5 +0,0 @@
-{
-  name = "mastoproxy";
-  version = "0.1.0";
-  dependencies = {};
-}
